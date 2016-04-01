2e2b848292b9668bcb1f7c424bafba91f53dddd4
Yogaesh Ramasundaram <yramasundaram@rchmov105.local>
2016-04-02 09:11:49 +1100

    Boiler plate spring boot application with gitignore and blank README.md file

---
+++ src/main/java/com/discountify/DiscountifyApplication.java
@@ -0,0 +1,12 @@
+package com.discountify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountifyApplication.class, args);
+	}
+}
